39d8f7214b5cacd04db33e079537ee90a05f5662
braga.guilherme <braga.guilherme@ifood.com.br>
2025-01-25 23:05:40 -0300

    di texts

--- dependecy-injection/examples/2-good-code.ts
+++ dependecy-injection/examples/2-good-code.ts
@@ -33,11 +33,21 @@ class Car {
     }
 }
 
+// Inversão de Controle: a Classe DEPENDE do objeto, mas ela não sabe do que ele precisa para ser instanciado, logo, ela deixa com que a aplicação decida.
+    // Assim, não há necessidade em controlar os argumentos das dependências e as dependência em si;
+    // Imagine a Inversão de Controle como uma receita de um bolo:
+        // - Assadeira: Classe Principal
+        // - Bolo: Dependência
+        // - Ingredientes: Argumentos das Dependências
+    // A Injeção de Dependência nos permite criar qualquer tipo de Bolo a partir dessa Assadeira, sem nem sequer se preocupar com os Ingredientes.
+        // Me dê um Bolo de Morango -> Classe de implementação real
+        // Me dê um Bolo de Chocolate -> Classe de mocking para testes
+
 // As 2 Principais Vantagens da DI:
-// 1. Teste: A Classe Car não precisa conhecer as dependências das Classes Engines
-    // 1.1 Para testar a classe Car, é possível mockar as Classes Engines sem se preocupar com as dependências
+// 1. Teste: A Classe Car não precisa conhecer as dependências das Classes Engines 
+    // 1.1 Para testar a Classe Car, é possível mockar as Classes Engines sem se preocupar com as dependências
 // 2. Implementação: A Classe Car não precisa controlar os argumentos das Classes Engine
-    // Se há alteração nos argumentos das Classes Engines, então a Classe Car precisa ajustar seus argumentos
+    // 2.1 Se há alteração nos argumentos das Classes Engines, então a Classe Car precisa ser ajustada para repassar esses argumentos
 
     // Conclusão>>> DI = Desacoplamento e Testabilidade
 
@@ -46,6 +56,16 @@ class Car {
         // Não é necessário conhecer como as dependências são instanciadas
 const mockGasEngine = { start: () => console.log("Mock gas engine started") };
 const mockElectricEngine = { start: () => console.log("Mock electric engine started") };
+
+// Inversão de Controle: a instância do objeto Car é quem controla as dependências
 const car = new Car(mockGasEngine as GasEngine, mockElectricEngine as ElectricEngine);
 car.startHybrid();
 
+// Conclusão
+    // Injeção de dependência: dependências se tornam argumentos 
+    // constructor(
+    //     private gasEngine: GasEngine,
+    //     private eletricEngine: ElectricEngine
+    // )
+    // Inversão de Controle: a instância da classe injetada controla as dependências
+    // new Car(mockGasEngine as GasEngine, mockElectricEngine as ElectricEngine)
